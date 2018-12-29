86f26cb1b6541f0fbc2ba64d284305dc877eed33
thss15_liux <liuxiang15@mails.tsinghua.edu.cn>
2018-12-29 15:22:27 +0800

    实现了性别的饼状图显示

--- homework/solution.py
+++ homework/solution.py
@@ -2,6 +2,8 @@ import pandas as pd
 import numpy as np
 from pandas import Series,DataFrame
 
+import matplotlib.pyplot as plt
+
 def preProcess():
 	data = pd.read_csv("diabetic_data.csv")
 
@@ -20,6 +22,34 @@ def preProcess():
 	#将 discharge_disposition_id 字段为11，13，14，19，20，21的记录过滤掉
 	data = data[~data["discharge_disposition_id"].isin([11, 13, 14, 19, 20, 21])]
 	print("此时数据的行数为："+ str(data.index.size))
+	graph(data)
+
+def graph(data):
+	#以图表形式统计余下病人年龄、种族、性别、入院类型、入院初诊（diag_1）、出院去向等信息分布情况
+	gender_data = data.loc[:, ['gender']]
+	# gender_num = print(gender_data.apply(pd.value_counts))
+	# print(gender_num)
+	gender_num = gender_data.ix[:,"gender"]
+	#print(gender_num)
+	gender_result = pd.value_counts(gender_num.values, sort=False) #索引被重新定义的series数组
+	print(gender_result)
+	print(gender_result.index)
+	print(gender_result.values)
+
+
+	#绘制gender饼状图
+	
+	#label = ["Female", "Male", "Unknown"]#定义饼图的标签，标签是列表
+	# 调节图形大小，宽，高
+	plt.figure(figsize=(9, 6))
+	labels = gender_result.index
+	explode = [0.01, 0.01, 0.01]  # 设定各项距离圆心n个半径
+	#fig = plt.figure(figsize=(6, 6))# 将画布设定为正方形，则绘制的饼图是正圆
+	plt.pie(gender_result.values, labels=labels, autopct='%1.4f%%',explode=explode)#画饼图（数据，数据对应的标签，百分数保留两位小数点）
+	plt.title("gender pie chart")
+	plt.show()
+	plt.savefig("GenderPieChart.jpg")
+
 
 preProcess()
 
